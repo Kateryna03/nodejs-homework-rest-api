7b4ffddeac3679b4dae27dcb8c3de12cfa76a2c2
Kateryna Gurienkova <gurienkova@gmail.com>
2021-10-27 00:55:28 +0300

    Исправила валидацию - добавила return при не валидных данных

--- middlewares/validation/contacts.js
+++ middlewares/validation/contacts.js
@@ -18,8 +18,45 @@ module.exports = {
     });
     const validationResult = schema.validate(req.body);
     if (validationResult.error) {
-      res.status(400).json({ status: validationResult.error.details });
+      res.status(400).json({
+        status: "error",
+        code: 400,
+        message: validationResult.error.message,
+      });
+      return; // не забывать возврашать при ошибке валидации, чтобы не попадал невалидный контакт в базу
     }
     next();
   },
 };
+// const schemaAddNewCon = Joi.object({
+//   name: Joi.string().alphanum().min(3).max(30).required(),
+
+//   email: Joi.string()
+//     .email({
+//       minDomainSegments: 2,
+//       tlds: { allow: ["com", "net", "ua"] },
+//     })
+//     .required(),
+
+//   phone: Joi.number().required(),
+// });
+
+// const validation = (schema) => {
+//   return async (req, res, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       res.status(400).json({
+//         status: "error",
+//         code: 400,
+//         message: error.message,
+//       });
+//       return;
+//     }
+//     next();
+//   };
+// };
+
+// module.exports = {
+//   validation,
+//   schemaAddNewCon,
+// };
