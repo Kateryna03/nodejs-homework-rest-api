23dbfdbdaa8f60ee935d9727229ba1cb918e1baf
Kateryna Gurienkova <gurienkova@gmail.com>
2021-11-22 22:13:44 +0200

    + jipm(min 250x250)

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -11,13 +11,15 @@
 const fs = require("fs/promises");
 const { url } = require("inspector");
 const path = require("path");
+const Jimp = require("jimp");
 //const { BadRequest, NotFound, Unauthorized } = require("http-errors");
 const { User } = require("../../db");
 //const { SECRET_KEY } = process.env;
 
 const avatarDir = path.join(__dirname, "../../public/avatars");
-console.log(avatarDir);
+console.log("ПУТЬ К ПАПКЕ АВАТАРОК", avatarDir);
 
+//avatarURL.resize(250, 250, Jimp.RESIZE_BEZIER);
 const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
   try {
@@ -37,19 +39,38 @@ const updateAvatar = async (req, res, next) => {
     //const { id } = req.params;
 
     // const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-    const resultUpload = path.join(avatarDir, id, `${id}_${originalname}`);
+    const resultUpload = path.join(avatarDir, id, `${id}${originalname}`);
+    //const resultUpload = path.join(avatarDir, originalname);
+    console.log(resultUpload);
+    //const imgPath = req.file.path;
+    //const image = `${id}${originalname}`;
+    Jimp.read(resultUpload, (err, image) => {
+      if (err) throw err;
+      image
+        .resize(250, 250) // resize
+        //   .quality(60) // set JPEG quality
+        //   .greyscale() // set greyscale
+        .write(`${id}${originalname}`); // save
+    });
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("/public/avatars", id, `${id}_${originalname}`);
+    //const avatarURL = path.join("/public/avatars", id, originalname);
+    const avatarURL = path.join("/public/avatars", id, `${id}${originalname}`);
+    console.log(avatarURL);
     const result = await User.findByIdAndUpdate(
       id,
       { avatarURL },
       { new: true }
     );
+    if (!result) {
+      res.status(401).json({ message: "No AVATARS" });
+      //console.log("NO AVATARS");
+    }
+    console.log("RESULT", result);
     res.json({
       status: "success",
       code: 200,
       data: {
-        result,
+        avatarURL,
       },
     });
   } catch (error) {
